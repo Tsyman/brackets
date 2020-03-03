ed66132cf904f7f1a1862bd58d272faaba0ebfba
Max Tsyman <xxxmaximxxx777@gmail.com>
2020-03-03 21:53:44 +0300

    feat: add brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    
+    let newConfigArr = bracketsConfig.map(function(item) {
+        return item.join("");
+    })
+
+    let reducedLength = str.length / 2;
+    let i = 0;
+    while (i < reducedLength) {
+        for (let j = 0; j < newConfigArr.length; j++) {
+            str = str.replace(newConfigArr[j], "");
+        }
+        i++;
+    } 
+    
+    return (str.length == 0) ? true : false;
 }
